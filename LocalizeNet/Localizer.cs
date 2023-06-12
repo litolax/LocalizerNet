82482bcb87e3dd4b6612dd8a88cf36219f393616
Yuri Ratkevich <ura_rat@mail.ru>
2023-06-12 22:05:44 +0300

    Simple key/token system

--- LocalizeNet/Localizer.cs
+++ LocalizeNet/Localizer.cs
@@ -1,16 +1,47 @@
-﻿namespace LocalizeNet;
+﻿using System.Text.Json;
+
+namespace LocalizeNet;
 
 public class Localizer
 {
-    private readonly string _localization;
+    private readonly JsonDocument _localization;
 
     public Localizer(string localization)
     {
-        this._localization = localization;
+        this._localization = JsonDocument.Parse(localization);
     }
 
-    public string GetLocalization()
+    public string GetString(string key)
     {
-        return this._localization;
+        JsonElement token;
+
+        if (TryGetToken(key, out token))
+        {
+            return token.GetString() ?? key;
+        }
+
+        return key;
+    }
+
+    private bool TryGetToken(string key, out JsonElement token)
+    {
+        string[] keys = key.Split(':');
+        JsonElement root = this._localization.RootElement;
+
+        foreach (string k in keys)
+        {
+            if (root.TryGetProperty(k, out JsonElement child))
+            {
+                root = child;
+            }
+            else
+            {
+                token = default;
+                return false;
+            }
+        }
+
+        token = root;
+        return true;
     }
 }
